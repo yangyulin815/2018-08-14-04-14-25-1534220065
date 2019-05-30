89de1d119067b145e4d425dc751540105fcf2188
yangyulin <yyl815@163.com>
2019-05-30 17:39:03 +0800

    add lcd

--- main/main.js
+++ main/main.js
@@ -1,4 +1,55 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(x) {  
+	var len=x.length; 
+    var arr_s=[
+    "._.",
+    "...",
+    "._.",
+    "._.",
+    "...",
+    "._.",
+    "._.",
+	"._.",
+	"._.",
+	"._."];
+ 
+    var arr_z=[
+    "|.|",
+    "..|",
+    "._|",
+    "._|",
+    "|_|",
+    "|_.",
+    "|_.",
+	"..|",
+	"|_|",
+	"|_|"];
+
+    var arr_x=[
+    "|_|",
+    "..|",
+    "|_.",
+    "._|",
+    "..|",
+    "._|",
+    "|_|",
+	"..|",
+	"|_|",
+	"..|"
+    ];
+ 
+    var res1='',res2='',res3='',res='';
+ 	
+ 	for(var i = 0 ; i < len - 1 ; i++){
+ 		res1+=arr_s[x[i]]+" ";
+ 		res2+=arr_z[x[i]]+" ";
+ 		res3+=arr_x[x[i]]+" ";
+ 	}
+
+ 	res=res1+arr_s[x[len-1]]+"\n"
+ 		+res2+arr_z[x[len-1]]+"\n"
+ 		+res3+arr_x[x[len-1]]+"\n";
+ 	
+ 	return res;
 };
+
+
